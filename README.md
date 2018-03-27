dcbba55282452ad911095a69fdbe73a21f56485d
Jonathan Ling <jonathan.ling@aptean.com>
2018-03-27 14:45:27 -0400

    Add readme, basic redirection route.

---
+++ README.md
@@ -0,0 +1,3 @@
+# shawty
+
+A small url shortener service written so that I could learn Flask.
--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask,redirect
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 	return 'Hello, World!'
+
+@app.route('/<link_id>')
+def redirect_to_route(link_id):
+	print(link_id)
+	return redirect('https://www.example.com', code=302)
