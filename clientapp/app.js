123e25ca500855fc94e8ba6732975ea0fff09801
Jonathan Ling <jonathan.ling@aptean.com>
2018-05-11 14:34:29 -0400

    Add basic parcel setup. Should get harder as time goes on.

--- clientapp/app.js
+++ clientapp/app.js
@@ -1,2 +1,7 @@
 import Vue from 'vue';
+import App from './src/App';
 
+new Vue({
+    el: "#app",
+    render: h => h(App),
+});
---
+++ clientapp/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app-root">This is a component! Here's {{ adjective }} message!</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    computed:{
+        adjective() : String {
+            return "a blah";            
+        }
+
+    }
+  
+})
+</script>
+
